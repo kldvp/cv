95a68bd3a6a2b49824d725956c111080d5c51211
kldvp <prasadkodali786@gmail.com>
2024-03-16 18:56:10 +0530

    feat: disable projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,31 +13,32 @@ export const metadata: Metadata = {
   description: RESUME_DATA.summary,
 };
 
-interface Projects {
-  projects: Array<Object>;
-}
+// TODO: fix projects section later
+// interface Projects {
+//   projects: Array<Object>;
+// }
 
-function RenderProjects({ projects }: Projects) {
-  if (!projects.length) return '';
-  return (
-    <Section className="print-force-new-page scroll-mb-16">
-      <h2 className="text-xl font-bold">Projects</h2>
-      <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
-        {projects.map((project: any) => {
-          return (
-            <ProjectCard
-              key={project.title}
-              title={project.title}
-              description={project.description}
-              tags={project.techStack}
-              link={"link" in project ? project.link.href : undefined}
-            />
-          );
-        })}
-      </div>
-    </Section>
-  )
-}
+// function RenderProjects({ projects }: Projects) {
+//   if (!projects.length) return '';
+//   return (
+//     <Section className="print-force-new-page scroll-mb-16">
+//       <h2 className="text-xl font-bold">Projects</h2>
+//       <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
+//         {projects.map((project: any) => {
+//           return (
+//             <ProjectCard
+//               key={project.title}
+//               title={project.title}
+//               description={project.description}
+//               tags={project.techStack}
+//               link={"link" in project ? project.link.href : undefined}
+//             />
+//           );
+//         })}
+//       </div>
+//     </Section>
+//   )
+// }
 
 export default function Page() {
   return (
@@ -198,7 +199,7 @@ export default function Page() {
             })}
           </div>
         </Section>
-        <RenderProjects projects={RESUME_DATA.projects}></RenderProjects>
+        {/* <RenderProjects projects={RESUME_DATA.projects}></RenderProjects> */}
       </section>
     </main>
   );
