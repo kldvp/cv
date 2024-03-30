67fdb7e5a9b9d8c79577a1aeba3f7d085a219e03
kldvp <prasadkodali786@gmail.com>
2024-03-30 08:41:14 +0530

    Fix typo

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -71,7 +71,7 @@ export const RESUME_DATA = {
       end: "2024",
       experiencePeriod: "December, 2019 - Current",
       description: `
-      I'm working as a Backend Developer for an HRM tool, I utilized Node.js and Loopback 3 to develop core tasks and REST APIs. 
+      I'm working as a Backend Developer for a HRM tool, I utilized Node.js and Loopback 3 to develop core tasks and REST APIs. 
       My contributions included implementing i18n translations, MongoDB, MongoDB Atlas search support for filters, workflows, redis background jobs, data export, and localized date formatting. 
       I also enhanced existing features, fixed bugs, and ensured code quality through unit testing with Jest and e2e testing with Ghost Inspector.
       `,
